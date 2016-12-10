34d30613271f261b59259ce3968335d7d8fcbeac
impboy <impboy@sbcglobal.net>
2016-12-09 18:01:16 -0800

    more mods of settings and urls.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3zd*ar@=%1=dtrey%%#1*v7#cl3k^9d-&riztabnd^f&3d983z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
